2e3a50e98691f16ecce11c40197b5f14ff739702
Aaron Gomez <aaron_gomez@grupoatic.com.mx>
2026-09-14 22:38:04 -0600

    Corregir rutas para GitHub Pages (basename + 404 fallback)

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import { AppDataProvider } from './context/AppDataContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AppDataProvider>
         <App />
       </AppDataProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
